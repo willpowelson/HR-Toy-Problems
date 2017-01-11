1d6115802179d385aad4d9b294f0e52ca5b4d63c
William Powelson <willpowelson@gmail.com>
2017-01-10 19:56:59 -0800

    Interleaved Strings created

---
+++ InterleavedStrings/InterleavedStrings.js
@@ -0,0 +1,24 @@
+/*
+* Given two words return an array of all interleavings
+*
+* An interleaving of two strings is a string containing all
+* characters from both strings but maintains the order of 
+* characters from a given string relative to other letters from
+* that same string:
+*
+* So if we interleaved 'AB' and 'CD' then 'BCDA' would not be valid
+* because B came after A in the original string but before in the
+* second string. 
+*
+* e.g. interleavedStrings('AB', 'CD') = [ 'ABCD', 'ACBD', 'ACDB', 'CABD', 'CADB', 'CDAB' ]
+*      interleavedStrings('it', 'do') = [ 'itdo', 'idto', 'idot', 'dito', 'diot', 'doit' ]
+*/
+
+
+//YOUR CODE HERE
+var interleavedStrings = function(first, second) {
+
+}
+
+//ignore this
+module.exports = interleavedStrings;
