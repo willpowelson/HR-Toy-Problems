a9e5c0110266e3dc07a35382a4a9deb58843ec0a
William Powelson <willpowelson@gmail.com>
2017-01-24 19:47:42 -0800

    problems 1/24/17

---
+++ LongestCommonSubstring/LongestCommonSubstring.js
@@ -0,0 +1,10 @@
+/*
+Given two strings, find the longest common substring
+
+LongestCommonSubstring('hooobil', 'dooby') === 'oob'
+*/
+
+
+var LongestCommonSubstring = function(str1, str2) {
+
+};
---
+++ StringIsomorphism/StringIsomorphism.js
@@ -0,0 +1,15 @@
+/*
+A string is considered isomorphic to a second string if there is a
+one to one mapping possible for every character of string one to every 
+character of string two
+
+StringIsomorphism('xxy', 'aab') === true
+
+StringIsomorphism('hello', 'zippy') === true
+*/
+
+
+//Your code here
+var StringIsomorphism = function(str1, str2) {
+
+};
